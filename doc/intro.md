abf7e927886c41a35d129da20635539d9a93abcf
kater-s <kate.ross-smith@ovoenergy.com>
2017-10-05 13:28:30 +0100

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cocktail-optimiser
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/cocktail_optimiser/core_test.clj
@@ -0,0 +1,7 @@
+(ns cocktail-optimiser.core-test
+  (:require [clojure.test :refer :all]
+            [cocktail-optimiser.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
